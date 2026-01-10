2497bb4f0dac9b85eda3f7d546080297cf22ba1e
Baafi Marcus <baafi.marcus@example.com>
2026-01-10 14:20:09 +0000

    fix: register UploadsController to enable visual menu delivery from database

--- src/system/system.module.ts
+++ src/system/system.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SystemController } from './system.controller';
+import { UploadsController } from './uploads.controller';
 import { SystemService } from './system.service';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
     imports: [PrismaModule],
-    controllers: [SystemController],
+    controllers: [SystemController, UploadsController],
     providers: [SystemService],
     exports: [SystemService],
 })
